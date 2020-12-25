8d3a1445bf6f7eeff7271d01b8f5d1be33c1d334
tovade <66245404+tovade@users.noreply.github.com>
2020-12-25 14:57:56 +0100

    ha ez
    dont take the config file serious its invalid

--- db.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require("salvage.db");
-const db = new MongoClient({
-  mongoURI: require("./token.json").Mongo,
-  schema: {
-    name: "tovade",
-  },
-});
-module.exports = db;
