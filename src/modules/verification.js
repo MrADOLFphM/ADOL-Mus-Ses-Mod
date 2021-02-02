b58277f7a22834f34c6c7f9838fa507c628f0b1c
tovade <66245404+tovade@users.noreply.github.com>
2021-02-02 18:56:41 +0100

    :bug: fix verification thing

--- src/modules/verification.js
+++ src/modules/verification.js
@@ -1,44 +1,42 @@
 const { MessageEmbed } = require("discord.js");
 
-module.exports = (client, message) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const mdb = await message.guild.getConfig();
-      const db = await message.guild.getVerify();
-      const mem = await client.getMember(message.guild, message.member);
-      if (!db?.enabled || !db?.role || !db?.channel || !db?.type) return false;
-      const channel_id = db?.channel;
-      const type = db?.type;
-      const role = db?.role;
-      const enabled = db?.enabled;
-      if (!enabled || !role || !channel_id) return false;
-      if (channel_id !== message.channel.id) return resolve(false);
-      if (message.deletable) message.delete({ timeout: 1000 }).catch(() => {});
-      if (type === "react") return resolve(false);
-      let verified = false;
-      if (type === "discrim") {
-        verified = message.content === `I am ${message.author.discriminator}`;
-      } else if (type === "captcha") {
-        const cptcha = mem.captcha;
-        verified = message.content === `${mdb.prefix}verify ${cptcha}`;
-      }
-      if (!verified) return;
-      message.channel
-        .send(
-          new MessageEmbed()
-            .setColor(0x00ff00)
-            .setTimestamp()
-            .setDescription(`<@${message.author.id}>, you have been verified!`)
-        )
-        .then((m) => {
-          m.delete({ timeout: 5000 }).catch(() => {});
-          if (!message.member.roles.cache.has(role))
-            message.member.roles.add(role).catch(() => {});
-          resolve(true);
-        })
-        .catch(() => {});
-    } catch (e) {
-      reject(e);
+module.exports = async (client, message) => {
+  try {
+    const mdb = await message.guild.getConfig();
+    const db = await message.guild.getVerify();
+    const mem = await client.getMember(message.guild, message.member);
+    if (!db?.enabled || !db?.role || !db?.channel || !db?.type) return false;
+    const channel_id = db?.channel;
+    const type = db?.type;
+    const role = db?.role;
+    const enabled = db?.enabled;
+    if (!enabled || !role || !channel_id) return false;
+    if (channel_id !== message.channel.id) return false;
+    if (message.deletable) message.delete({ timeout: 1000 }).catch(() => {});
+    if (type === "react") return false;
+    let verified = false;
+    if (type === "discrim") {
+      verified = message.content === `I am ${message.author.discriminator}`;
+    } else if (type === "captcha") {
+      const cptcha = mem.captcha;
+      verified = message.content === `${mdb.prefix}verify ${cptcha}`;
     }
-  });
+    if (!verified) return false;
+    message.channel
+      .send(
+        new MessageEmbed()
+          .setColor(0x00ff00)
+          .setTimestamp()
+          .setDescription(`<@${message.author.id}>, you have been verified!`)
+      )
+      .then((m) => {
+        m.delete({ timeout: 5000 }).catch(() => {});
+        if (!message.member.roles.cache.has(role))
+          message.member.roles.add(role).catch(() => {});
+        return true;
+      })
+      .catch(() => {});
+  } catch (e) {
+    return false;
+  }
 };
