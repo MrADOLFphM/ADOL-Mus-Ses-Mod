65b9b22c624ee571270e3ff1d9074c0be256bd32
tovade <66245404+tovade@users.noreply.github.com>
2021-03-21 18:00:32 +0100

    :tada: Programming quote command

---
+++ src/commands/fun/programming.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+module.exports = {
+  name: "pogramming",
+  aliases: ["pquote"],
+  description: "Random programming quote (i geuss).",
+  run: async (client, message, args) => {
+    const quote = await fetch(
+      "https:/programming-quotes-api.herokuapp.com/quotes/random"
+    ).then((r) => r.json());
+    const embed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(quote.en)
+      .setFooter(`By ${quote.author}`);
+    message.send(embed);
+  },
+};
