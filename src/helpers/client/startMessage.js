16a43131b798e018e15dee15d5f4de22a6a226d7
tovade <66245404+tovade@users.noreply.github.com>
2021-03-21 16:30:44 +0100

    :bug: fix: Await bug

--- src/helpers/client/startMessage.js
+++ src/helpers/client/startMessage.js
@@ -1,39 +1,39 @@
-module.exports = (client) => {
-    if (!client.config.dev) {
-        const bot = client.user.username;
-        const icon = client.emotes.success;
-        const servers = client.utils.formatNumber(client.guilds.cache.size);
-        const members = client.utils.formatNumber(
-          client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
-        );
-        const commands = client.commands.size;
-        const boot = client.bootTime;
-        const message = `${icon} \`[ ${client.version} ]\` **REBOOT**`;
-        const embed = {
-          color: "GREY",
-          description: [
-            "```properties",
-            `Servers: ${servers}`,
-            `Members: ${members}`,
-            `Command: ${commands}`,
-            `Boot: ${boot}ms`,
-            "```",
-          ].join("\n"),
-        };
-  
-        await client.channels.cache
-          .get("803706448784785488")
-          ?.createWebhook(bot, {
-            avatar: client.user.displayAvatarURL({
-              format: "png",
-              dynamic: true,
-              size: 128,
-            }),
-          })
-          .then((webhook) =>
-            Promise.all([webhook.send(message, { embeds: [embed] }), webhook])
-          )
-          .then(([_, webhook]) => webhook.delete())
-          .catch(() => {});
-      }
-}
+module.exports = async (client) => {
+  if (!client.config.dev) {
+    const bot = client.user.username;
+    const icon = client.emotes.success;
+    const servers = client.utils.formatNumber(client.guilds.cache.size);
+    const members = client.utils.formatNumber(
+      client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
+    );
+    const commands = client.commands.size;
+    const boot = client.bootTime;
+    const message = `${icon} \`[ ${client.version} ]\` **REBOOT**`;
+    const embed = {
+      color: "GREY",
+      description: [
+        "```properties",
+        `Servers: ${servers}`,
+        `Members: ${members}`,
+        `Command: ${commands}`,
+        `Boot: ${boot}ms`,
+        "```",
+      ].join("\n"),
+    };
+
+    await client.channels.cache
+      .get("803706448784785488")
+      ?.createWebhook(bot, {
+        avatar: client.user.displayAvatarURL({
+          format: "png",
+          dynamic: true,
+          size: 128,
+        }),
+      })
+      .then((webhook) =>
+        Promise.all([webhook.send(message, { embeds: [embed] }), webhook])
+      )
+      .then(([_, webhook]) => webhook.delete())
+      .catch(() => {});
+  }
+};
