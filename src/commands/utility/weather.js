411ca6b77b83ea15f341e442c0244f288f266602
tovade <66245404+tovade@users.noreply.github.com>
2021-04-17 14:08:26 +0200

    :bug: fix weather command (hopefully)

--- src/commands/utility/weather.js
+++ src/commands/utility/weather.js
@@ -8,45 +8,47 @@ module.exports = {
   description: "Shows weather of a city",
   usage: "[city name]",
   run: async (bot, message, args) => {
-    if (bot.config.dblkey.length === 0) {
-      message.channel.send("Your gay");
-      throw new Error("Nothing");
-    }
-    if (!args[0]) return message.channel.send("**Please Enter A City Name!**");
+    try {
+      if (!args[0])
+        return message.channel.send("**Please Enter A City Name!**");
 
-    weather.find(
-      { search: args.join(" "), degreeType: "C" },
-      function (err, result) {
-        if (err) message.channel.send(err.message);
+      weather.find(
+        { search: args.join(" "), degreeType: "C" },
+        function (err, result) {
+          if (err) message.channel.send(err.message);
 
-        if (result.length === 0) {
-          return message.channel.send("**Please Enter A Valid Location.**");
+          if (result.length === 0 || !results) {
+            return message.channel.send("**Please Enter A Valid Location.**");
+          }
+
+          var current = result[0].current;
+          var location = result[0].location;
+
+          const embed = new MessageEmbed()
+            .setDescription(`**${current.skytext}**`)
+            .setAuthor(`Weather for ${current.observationpoint}`)
+            .setThumbnail(current.imageUrl)
+            .setColor("GREEN")
+            .addField("**Timezone**", `UTC ${location.timezone}`, true)
+            .addField("**Degree Type**", `${location.degreetype}`, true)
+            .addField("**Temperature**", `${current.temperature} Degrees`, true)
+            .addField("**Feels Like**", `${current.feelslike} Degrees`, true)
+            .addField("**Winds**", `${current.winddisplay}`, true)
+            .addField("**Humidity**", `${current.humidity}%`, true)
+            .addField("**Date**", `${current.date}`, true)
+            .addField("**Day**", `${current.day}`, true)
+            .setFooter(
+              message.member.displayName,
+              message.author.displayAvatarURL()
+            )
+            .setTimestamp();
+
+          message.channel.send({ embed });
         }
-
-        var current = result[0].current;
-        var location = result[0].location;
-
-        const embed = new MessageEmbed()
-          .setDescription(`**${current.skytext}**`)
-          .setAuthor(`Weather for ${current.observationpoint}`)
-          .setThumbnail(current.imageUrl)
-          .setColor("GREEN")
-          .addField("**Timezone**", `UTC ${location.timezone}`, true)
-          .addField("**Degree Type**", `${location.degreetype}`, true)
-          .addField("**Temperature**", `${current.temperature} Degrees`, true)
-          .addField("**Feels Like**", `${current.feelslike} Degrees`, true)
-          .addField("**Winds**", `${current.winddisplay}`, true)
-          .addField("**Humidity**", `${current.humidity}%`, true)
-          .addField("**Date**", `${current.date}`, true)
-          .addField("**Day**", `${current.day}`, true)
-          .setFooter(
-            message.member.displayName,
-            message.author.displayAvatarURL()
-          )
-          .setTimestamp();
-
-        message.channel.send({ embed });
-      }
-    );
+      );
+    } catch (err) {
+      message.channel.send(err.message);
+      throw Error(err.message);
+    }
   },
 };
