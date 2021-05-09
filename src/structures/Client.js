49a4bc4c7f2290a6f3e0506d1703376c1de1edae
tovade <66245404+tovade@users.noreply.github.com>
2021-05-09 13:35:52 +0200

    :bug: fix intents

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -24,7 +24,19 @@ module.exports = class AndoiClient extends Client {
     super({
       disableMentions: "everyone",
       partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"],
-      intents: Intents.ALL,
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS,
+        Intents.FLAGS.GUILD_INTEGRATIONS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        Intents.FLAGS.GUILD_VOICE_STATES,
+        Intents.FLAGS.GUILD_WEBHOOKS,
+      ],
     });
     const player = new Player(this, {
       leaveOnEmpty: true,
