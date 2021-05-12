a41ea64c117bf73d67bad0a658c0e46fde492939
tovade <66245404+tovade@users.noreply.github.com>
2021-05-12 10:56:16 +0200

    :tada: change ping logs channel

--- src/helpers/client/pingChecker.js
+++ src/helpers/client/pingChecker.js
@@ -1,10 +1,11 @@
 module.exports = (client) => {
   setInterval(async () => {
     if (client.ws.ping > 5000) {
-      const channel = await client.channels.cache.get("804757935803990026");
+      const channel = await client.channels.cache.get("803706448784785488");
 
       const msg = `${client.emotes.warn} The bots ping is currently very high expect lower response time (${client.ws.ping}ms)`;
-      channel.send(msg);
+      const sended = await channel.send(msg);
+      sended.crosspost();
     }
   }, 30000);
 };
